ae11ea7296f06e347fe7b5b6961fef72c69fb87b
NinjaAiden <aidenharvey102@gmail.com>
2023-01-17 20:53:41 +0000

    Add files via upload

---
+++ section two/Decimals.java
@@ -0,0 +1,14 @@
+public class Decimals {
+    public static void main(String[] args) {
+        
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up");
+
+        double wallet = 20;
+        int people = 3;
+
+        double money = wallet / people;
+        System.out.println(money);
+
+    }
+}
