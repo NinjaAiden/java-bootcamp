e7e9b45ebd8616bdde864050405712613d14047f
NinjaAiden <aidenharvey102@gmail.com>
2023-02-06 16:09:43 +0000

    Add files via upload
    initial commit

---
+++ Hangman.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Hangman {
+
+    public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+    "wombat", "zebra"};
+
+    public static String[] gallows = {"+---+\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|   |\n" +
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + 
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" +
+    "/    |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + 
+    "/ \\  |\n" +
+    "     |\n" +
+    " =========\n"};
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        // get random word from list
+        String randomWord = words[(int)(Math.random() * words.length)];
+
+        // call printGameScreen
+        printGameScreen(randomWord);
+
+        scan.close();
+
+    }
+
+    public static void printGameScreen(String randomWord) {
+
+        // pass randomWord string to a char array
+        char word[] = new char[randomWord.length()];
+        for (int i = 0; i < randomWord.length(); i++) {
+            word[i] = randomWord.charAt(i);
+        }
+
+        //print starting gallows
+        System.out.println(gallows[0]);
+
+        System.out.print("Word: ");
+        for (int i = 0; i < word.length; i++) {
+            System.out.print("_ ");
+        }
+    
+
+        System.out.println("\n\nMisses: ");
+    }
+}
