d540f941cb1220af078a0b25ea14c3ed27bbccc8
NinjaAiden <aidenharvey102@gmail.com>
2023-05-08 15:50:03 +0100

    Final commit
    added loss conditions and reworked some parts for efficiency

--- Hangman.java
+++ Hangman.java
@@ -67,7 +67,8 @@ public class Hangman {
     "/|\\  |\n" + 
     "/ \\  |\n" +
     "     |\n" +
-    " =========\n"};
+    " =========\n"
+    };
 
     public static void main(String[] args) {
 
@@ -76,92 +77,148 @@ public class Hangman {
         // initialise counter for incorrect guesses
         int wrongGuessCount = 0;
         
-        char[] word = getWord();                    // get word by calling getWord()
-        char[] hiddenWord = new char[word.length];  // create new hidden word array
-        char[] missedGuesses = new char[6];         // create array for missed guesses. Max wrong turns is 6
+        String word = getWord();             // get word by calling getWord()
+        char[] hiddenWord = hideWord(word);  // create new hidden word array
+        char[] missedGuesses = new char[6];  // create array for missed guesses. Max wrong guesses is 6
         
-        //hide word
-        for (int i = 0; i < hiddenWord.length; i++) {
-            hiddenWord[i] = '_';
-        }   
-
         // call printGameScreen
         printGameScreen(hiddenWord, wrongGuessCount, missedGuesses);
 
-        while (wrongGuessCount <= 6){
+        while (wrongGuessCount < 6){
 
             char playerGuess = scan.next().charAt(0);
-            for (int i = 0; i < word.length; i++){
-                if (word[i] == playerGuess) {
-                    hiddenWord[i] = playerGuess;
-                } else {
-                    missedGuesses[wrongGuessCount] = playerGuess;
-                }
+            if(checkGuess(playerGuess, word)){
+                updatePlaceholders(playerGuess, hiddenWord, word);
+            } else {
+                missedGuesses[wrongGuessCount] = playerGuess;
+                wrongGuessCount++;
             }
 
+            //update game screen
             printGameScreen(hiddenWord, wrongGuessCount, missedGuesses);
 
             checkWin(hiddenWord, word);
 
-        }
+            if (wrongGuessCount >= 6){
 
-        
+                String revealedWord = new String(word);
 
+                System.out.println("\nThe man was hanged, bad luck");
+                System.out.println("the word was " + revealedWord); 
+            }                              
+        }            
         scan.close();
     }
 
     /**
      *  Function name - getWord
-     *  @return word (char[])
+     *  @return word (String)
      * 
      *  Inside the function:
      *      1. Get random word from list of words
      *      2. pass word from a String to a char array and return
      */
-   public static char[] getWord(){
+    public static String getWord(){
 
-    // get random word from list
-    String randomWord = words[(int)(Math.random() * words.length)];
+        // get random word from list
+        String word = words[(int)(Math.random() * words.length)];
 
-    // pass randomWord string to a char array
-    char word[] = new char[randomWord.length()];
-    for (int i = 0; i < randomWord.length(); i++) {
-        word[i] = randomWord.charAt(i);
+        return word;
     }
 
-    return word;
-   }
+    /**
+     * function name - hideWord 
+     * @param word (String)
+     * @return hiddenWord (char[])
+     * 
+     * inside the function:
+     *  1. create new char array, same length as the String word
+     *  2. change all letters to '_' to create placeholders
+     */
+    public static char[] hideWord(String word){
+
+        char[] hiddenWord = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++){
+            hiddenWord[i] = word.charAt(i);
+        }
+   
+        for (int i = 0; i < hiddenWord.length; i++) {
+            hiddenWord[i] = '_';
+        }   
+        return hiddenWord;
+    }
 
     /**
      * Function name - printGameScreen
-     *  @param word (char[])
-     *  @param int guesses
+     *  @param hiddenWord (char[])
+     *  @param wrongGuessCount int
+     *  @param missedGuesses (char[])
      * 
      *  Inside the function:
-     *      1. print gallows (use guessCount to access correct stage)
-     *      2. hide word and print empty spaces
+     *      1. print gallows (use wrongGuessCount to access correct stage)
+     *      2. print placeholders
      *      3. print missed guesses
-     *      4. Prompt for next guess
+     *      4. print prompt for next guess
      */
+    public static void printGameScreen(char[] hiddenWord, int wrongGuessCount, char[] missedGuesses) {        
 
-    public static void printGameScreen(char[] hiddenWord, int wrongGuessCount, char[] wrongLetters) {        
-
-        //print gallows
         System.out.println("\n" + gallows[wrongGuessCount]);
 
         System.out.print("Word: " + new String(hiddenWord));
 
-        System.out.println("\n\nMisses: " + new String(wrongLetters));
+        System.out.println("\n\nMisses: " + new String(missedGuesses));
 
         System.out.print("\nGuess: ");
     }
 
-    public static void checkWin(char[] hiddenWord, char[] word){
+   /**
+    * Function name - checkGuess
+    * @param playerGuess char
+    * @param word String
+    *
+    * Inside the function:
+    *   1. if player has guessed a correct letter return true
+    *   2. if player guess does not match any letters, return false
+    */
+    public static boolean checkGuess(char playerGuess, String word){
+
+        for (int i = 0; i < word.length(); i++){
+            if (word.charAt(i) == playerGuess) {
+                return true;                               
+            }
+        } return false;
+    }
+
+    /**
+     * function name - updatePlaceholders
+     * @param playerGuess
+     * @param hiddenWord
+     * @param word
+     */
+    public static void updatePlaceholders(char playerGuess, char[] hiddenWord, String word){
+        for(int j = 0; j < word.length(); j++){
+            if (word.charAt(j) == playerGuess){
+                hiddenWord[j] = playerGuess;
+            }
+        }        
+    }
+
+    /**
+     * Function name - checkWin
+     * @param hiddenWord char[]
+     * @param word char[]
+     * 
+     * Inside the function:
+     *      1. convert the char arrays into strings for easier comparison
+     *      2. check the two strings are the same
+     *      3. If the strings are the same, print win message and exit
+     */
+    public static void checkWin(char[] hiddenWord, String word){
 
         String hiddenWordString = new String(hiddenWord);
-        String wordString = new String(word);
 
-        if (hiddenWordString.equals(wordString)){
+        if (hiddenWordString.equals(word)){
             System.out.println("Congratulations! You win!");
             System.exit(0);
         }
