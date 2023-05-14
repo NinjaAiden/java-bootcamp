503058c91afd7549eaadab7e5b10268dfd783eb6
NinjaAiden <aidenharvey102@gmail.com>
2023-05-14 17:14:29 +0100

    added setters

--- section seven/Person.java
+++ section seven/Person.java
@@ -17,12 +17,24 @@ public class Person {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getNationality(){
         return nationality;
     }
 
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
     public String getDateOfBirth(){
         return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 
     public String[] getPassport(){
@@ -31,5 +43,9 @@ public class Person {
 
     public int getSeatNumber(){
         return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
     }
 }
