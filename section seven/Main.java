993cbcf837d7cfbe8be6ace12cefd0e312180c90
NinjaAiden <aidenharvey102@gmail.com>
2023-05-14 16:28:01 +0100

    cleaned code by adding constructors

--- section seven/Main.java
+++ section seven/Main.java
@@ -1,20 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2020;
-        nissan.color = "Green";     
+        Car nissan = new Car("Nissan", 10000, 2020, "Green");   
 
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "Blue";
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price + 
         ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
         System.out.println("This " + dodge.make + " is worth $" + dodge.price + 
         ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+
+        Person passenger = 
+            new Person("Mike", "English", "February 23rd 1987", new String[3], 227 );
+
+        System.out.println(passenger.name + " is " + passenger.nationality + ", date of birth is " + passenger.dateOfBirth
+        + ". Passport details are " + (Arrays.toString(passenger.passport)) + " and is sitting in seat number " + passenger.seatNumber);
     }
 }
--- section seven/Person.java
+++ section seven/Person.java
@@ -4,4 +4,12 @@ public class Person {
     String dateOfBirth;
     String[] passport;
     int seatNumber;
+
+    public Person(String name, String nationality, String dateOfBirth, String[] passport, int seatNumber){
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.passport = new String[]{name, nationality, dateOfBirth};
+        this.seatNumber = seatNumber;
+    }
 }
