21ef23a42f2d41600b6495c7c4e491a5e32b48fa
NinjaAiden <aidenharvey102@gmail.com>
2023-08-29 13:14:08 +0100

    added polymorphism constructors

--- section 14/Main.java
+++ section 14/Main.java
@@ -5,12 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Cylinder cylinder = new Cylinder();
-        cylinder.setHeight(2.0);
-        cylinder.setRadius(1.0);
+        Cylinder cylinder = new Cylinder(1.0, 2.0);
         
-        Sphere sphere = new Sphere();
-        sphere.setRadius(1.0);
+        Sphere sphere = new Sphere(1.0);
 
         System.out.println("Sphere - radius: " + sphere.getRadius());
         System.out.println("Cylinder - radius: " + cylinder.getRadius() + " height " + cylinder.getHeight());
