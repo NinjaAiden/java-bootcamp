0cb85e433257a8f8a4124cd7b34dffe4ddf9c70f
NinjaAiden <aidenharvey102@gmail.com>
2023-08-30 11:12:20 +0100

    Inheritance extra practice

---
+++ section 14 extra practice/model/Microscope.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Microscope extends LabEquipment {
+
+    private int magnification;
+    private static final int MIN_MAGNIFICATION = 1;
+
+    public Microscope(String manufacturer, String model, int year, int magnification){
+        super(manufacturer, model, year);
+        setMagnification(magnification);
+    }
+
+    public int getMagnification(){
+        return this.magnification;
+    }
+
+    public void setMagnification(int magnification){
+        if(magnification < MIN_MAGNIFICATION){
+            throw new IllegalArgumentException("Magnification must be greater than or equal to minimum magnification");
+        }
+        this.magnification = magnification;
+    }
+
+    @Override
+    public String performMaintenance(){
+        return "Microscope maintenance: clean the lenses and check light source";
+    }
+    
+}
